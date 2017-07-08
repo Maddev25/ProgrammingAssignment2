fd3b18d0a21f087963839f3080a685bb6b69d8e1
Maddev25 <devanb4u@gmail.com>
2017-07-09 00:26:17 +1000

    Final One

--- cachematrix.R
+++ cachematrix.R
@@ -2,28 +2,41 @@
 ## Matrix Inverse Caching and Lexical Scoping example
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-          x<<-y
-          m<<-NULL
-        }
-get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 ## To get the Cached Data if the matrix is not changed else recompute
-cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
-    }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
-    m
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+## Code to test the function 
+## testMatrix <- makeCacheMatrix(matrix(c(1,2,2,4,3,4,1,1,4),3,3))
+## testMatrix$get()
+## cacheSolve(testMatrix)
+## cacheSolve(testMatrix)
+## testMatrix <- makeCacheMatrix(matrix(1:4,2,2))
+## cacheSolve(testMatrix)
+## cacheSolve(testMatrix)
+
+
